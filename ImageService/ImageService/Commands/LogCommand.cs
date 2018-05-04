26739aa95d3c274aeaec1d3a6641a84c45d7e5a2
avihayarzuan <avihayarzuan@gmail.com>
2018-05-04 13:47:16 +0300

    LogCommand works

--- ImageService/ImageService/Commands/LogCommand.cs
+++ ImageService/ImageService/Commands/LogCommand.cs
@@ -1,6 +1,10 @@
 ﻿using ImageService.Commands;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +15,30 @@ namespace ImageService.ImageService.Commands
     {
         public string Execute(string[] args, out bool result)
         {
-            throw new NotImplementedException();
+            Dictionary<int, string[]> map = new Dictionary<int, string[]>();
+
+            string logName = ConfigurationManager.AppSettings["LogName"];
+            EventLog myLog = new EventLog(logName, ".");
+            EventLogEntry entry;
+            EventLogEntryCollection entries = myLog.Entries;
+
+            for (int i = (entries.Count -1); i > 0; i--)
+            {
+                entry = entries[i];
+                string[] str = new string[2];
+                str[0] = entry.EntryType.ToString();
+                str[1] = entry.Message.ToString();
+                int.TryParse(entry.InstanceId.ToString(), out int id);
+                map.Add(id,str);
+                if (id == 1)
+                {
+                    break;
+                }
+            }
+            string answer = JsonConvert.SerializeObject(map);
+
+            result = true;
+            return answer;
         }
     }
 }
