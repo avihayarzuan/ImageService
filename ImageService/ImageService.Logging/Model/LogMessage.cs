1b8b8206bfc9b4dd85a617346637bdcd2645f106
avihayarzuan <avihayarzuan@gmail.com>
2018-05-21 02:14:32 +0300

    a

---
+++ ImageService/ImageService.Logging/Model/LogMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.ImageService.Logging.Model
+{
+    public class LogMessage
+    {
+        public string Type { get; private set; }
+        public string Message { get; private set; }
+
+        public LogMessage(string type, string msg)
+        {
+            this.Type = type;
+            this.Message = msg;
+        }
+    }
+}
